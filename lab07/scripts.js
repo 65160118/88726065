faa6e902681db311da26c4288fd36bc2202075aa
65160118 <65160118@go.buu.ac.th>
2024-01-17 19:08:40 +0700

    toggle รายการ Todo

--- lab07/scripts.js
+++ lab07/scripts.js
@@ -26,6 +26,12 @@ function deleteTodo(index) {
     renderTodoList();
 }
 
+// ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodoList();
+}
+
 // แสดงรายการ Todo บนหน้าเว็บ
 function renderTodoList() {
     console.log(todos);
